b0008474243d99003d0331fda2f89c78f2e4fa20
Rosha10 <roshankarule@outlook.com>
2022-11-04 17:54:09 +0530

    Update Setting.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["bestcarsdjangoapp.us-east.cf.appdomain.cloud","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["bestcarsdjangoapp.us-east.cf.appdomain.cloud","127.0.0.1","localhost"]
 
 
 # Application definition
