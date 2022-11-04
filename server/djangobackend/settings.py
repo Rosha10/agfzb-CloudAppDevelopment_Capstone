8a129855164324ed61ff313c6af3d509c8e02299
Rosha10 <roshankarule@outlook.com>
2022-11-04 17:26:26 +0530

    Update Index.html

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["bestcarsdjangoapp.us-east.cf.appdomain.cloud","localhost"]
+ALLOWED_HOSTS = ["bestcarsdjangoapp.us-east.cf.appdomain.cloud","localhost","127.0.0.1"]
 
 
 # Application definition
